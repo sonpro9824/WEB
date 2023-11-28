646244dddbfaebc10dea69a18589d7e83dda5d39
Hson <nghongson9824@gmail.com>
2023-11-28 10:49:05 +0700

    10h49

--- Form2.Designer.cs
+++ Form2.Designer.cs
@@ -44,14 +44,14 @@
             this.button_next = new FontAwesome.Sharp.IconButton();
             this.iconButton1 = new FontAwesome.Sharp.IconButton();
             this.panel_thuMuc = new System.Windows.Forms.Panel();
-            this.button_xong = new System.Windows.Forms.Button();
-            this.button_xoa = new System.Windows.Forms.Button();
-            this.label9 = new System.Windows.Forms.Label();
-            this.iconButton2 = new FontAwesome.Sharp.IconButton();
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
-            this.textBox_thuMuc = new System.Windows.Forms.TextBox();
-            this.label10 = new System.Windows.Forms.Label();
             this.label11 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.textBox_thuMuc = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.iconButton2 = new FontAwesome.Sharp.IconButton();
+            this.label9 = new System.Windows.Forms.Label();
+            this.button_xoa = new System.Windows.Forms.Button();
+            this.button_xong = new System.Windows.Forms.Button();
             this.panel1.SuspendLayout();
             this.panel_thuMuc.SuspendLayout();
             this.SuspendLayout();
@@ -188,7 +188,7 @@
             // 
             this.dateTimePicker1.CustomFormat = "dd/MM/yyyy";
             this.dateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
-            this.dateTimePicker1.Location = new System.Drawing.Point(764, 23);
+            this.dateTimePicker1.Location = new System.Drawing.Point(771, 23);
             this.dateTimePicker1.Name = "dateTimePicker1";
             this.dateTimePicker1.Size = new System.Drawing.Size(96, 22);
             this.dateTimePicker1.TabIndex = 18;
@@ -201,7 +201,7 @@
             this.button_bookmark.IconChar = FontAwesome.Sharp.IconChar.Star;
             this.button_bookmark.IconColor = System.Drawing.Color.Black;
             this.button_bookmark.IconFont = FontAwesome.Sharp.IconFont.Auto;
-            this.button_bookmark.Location = new System.Drawing.Point(705, 7);
+            this.button_bookmark.Location = new System.Drawing.Point(711, 7);
             this.button_bookmark.Name = "button_bookmark";
             this.button_bookmark.Size = new System.Drawing.Size(50, 50);
             this.button_bookmark.TabIndex = 6;
@@ -264,39 +264,42 @@
             this.panel_thuMuc.Size = new System.Drawing.Size(351, 168);
             this.panel_thuMuc.TabIndex = 18;
             // 
-            // button_xong
+            // label11
             // 
-            this.button_xong.BackColor = System.Drawing.Color.Tan;
-            this.button_xong.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button_xong.Location = new System.Drawing.Point(186, 117);
-            this.button_xong.Name = "button_xong";
-            this.button_xong.Size = new System.Drawing.Size(59, 36);
-            this.button_xong.TabIndex = 0;
-            this.button_xong.Text = "Xong";
-            this.button_xong.UseVisualStyleBackColor = false;
-            this.button_xong.Click += new System.EventHandler(this.button_xong_Click);
+            this.label11.AutoSize = true;
+            this.label11.Font = new System.Drawing.Font("Calibri", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label11.Location = new System.Drawing.Point(14, 78);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(112, 21);
+            this.label11.TabIndex = 7;
+            this.label11.Text = "Thêm thư mục";
             // 
-            // button_xoa
+            // label10
             // 
-            this.button_xoa.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button_xoa.Location = new System.Drawing.Point(277, 117);
-            this.button_xoa.Name = "button_xoa";
-            this.button_xoa.Size = new System.Drawing.Size(59, 36);
-            this.button_xoa.TabIndex = 1;
-            this.button_xoa.Text = "Xóa";
-            this.button_xoa.UseVisualStyleBackColor = true;
-            this.button_xoa.Click += new System.EventHandler(this.button_xoa_Click);
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Calibri", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label10.Location = new System.Drawing.Point(14, 50);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(71, 21);
+            this.label10.TabIndex = 6;
+            this.label10.Text = "Thư mục";
             // 
-            // label9
+            // textBox_thuMuc
             // 
-            this.label9.AutoSize = true;
-            this.label9.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label9.Location = new System.Drawing.Point(14, 11);
-            this.label9.Name = "label9";
-            this.label9.Size = new System.Drawing.Size(167, 24);
-            this.label9.TabIndex = 2;
-            this.label9.Text = "Đã thêm dấu trang";
-            this.label9.Click += new System.EventHandler(this.label9_Click);
+            this.textBox_thuMuc.Location = new System.Drawing.Point(132, 77);
+            this.textBox_thuMuc.Name = "textBox_thuMuc";
+            this.textBox_thuMuc.Size = new System.Drawing.Size(204, 22);
+            this.textBox_thuMuc.TabIndex = 5;
+            this.textBox_thuMuc.TextChanged += new System.EventHandler(this.textBox_thuMuc_TextChanged);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(132, 47);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(204, 24);
+            this.comboBox1.TabIndex = 4;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
             // 
             // iconButton2
             // 
@@ -313,42 +316,39 @@
             this.iconButton2.UseVisualStyleBackColor = true;
             this.iconButton2.Click += new System.EventHandler(this.iconButton2_Click);
             // 
-            // comboBox1
+            // label9
             // 
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(132, 47);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(204, 24);
-            this.comboBox1.TabIndex = 4;
-            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(14, 11);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(167, 24);
+            this.label9.TabIndex = 2;
+            this.label9.Text = "Đã thêm dấu trang";
+            this.label9.Click += new System.EventHandler(this.label9_Click);
             // 
-            // textBox_thuMuc
+            // button_xoa
             // 
-            this.textBox_thuMuc.Location = new System.Drawing.Point(132, 77);
-            this.textBox_thuMuc.Name = "textBox_thuMuc";
-            this.textBox_thuMuc.Size = new System.Drawing.Size(204, 22);
-            this.textBox_thuMuc.TabIndex = 5;
-            this.textBox_thuMuc.TextChanged += new System.EventHandler(this.textBox_thuMuc_TextChanged);
+            this.button_xoa.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button_xoa.Location = new System.Drawing.Point(277, 117);
+            this.button_xoa.Name = "button_xoa";
+            this.button_xoa.Size = new System.Drawing.Size(59, 36);
+            this.button_xoa.TabIndex = 1;
+            this.button_xoa.Text = "Xóa";
+            this.button_xoa.UseVisualStyleBackColor = true;
+            this.button_xoa.Click += new System.EventHandler(this.button_xoa_Click);
             // 
-            // label10
+            // button_xong
             // 
-            this.label10.AutoSize = true;
-            this.label10.Font = new System.Drawing.Font("Calibri", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label10.Location = new System.Drawing.Point(14, 50);
-            this.label10.Name = "label10";
-            this.label10.Size = new System.Drawing.Size(71, 21);
-            this.label10.TabIndex = 6;
-            this.label10.Text = "Thư mục";
-            // 
-            // label11
-            // 
-            this.label11.AutoSize = true;
-            this.label11.Font = new System.Drawing.Font("Calibri", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label11.Location = new System.Drawing.Point(14, 78);
-            this.label11.Name = "label11";
-            this.label11.Size = new System.Drawing.Size(112, 21);
-            this.label11.TabIndex = 7;
-            this.label11.Text = "Thêm thư mục";
+            this.button_xong.BackColor = System.Drawing.Color.Tan;
+            this.button_xong.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button_xong.Location = new System.Drawing.Point(186, 117);
+            this.button_xong.Name = "button_xong";
+            this.button_xong.Size = new System.Drawing.Size(59, 36);
+            this.button_xong.TabIndex = 0;
+            this.button_xong.Text = "Xong";
+            this.button_xong.UseVisualStyleBackColor = false;
+            this.button_xong.Click += new System.EventHandler(this.button_xong_Click);
             // 
             // Form2
             // 
